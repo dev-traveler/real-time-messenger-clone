a48c52fede9360d2a58b28b222ccc906951212d8
Jaehyun Kim <avenj.dev@gmail.com>
2023-05-13 16:17:09 +0900

    feat: Add AuthForm Component

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { useCallback, useState } from 'react';
+import { SubmitHandler, FieldValues, useForm } from 'react-hook-form';
+
+import Input from '../../components/inputs/Input';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+      // Axios Register
+    }
+
+    if (variant === 'LOGIN') {
+      // NextAuth  Login
+    }
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="px-4 py-8 bg-white shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AuthForm;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,41 +1,22 @@
 import Image from 'next/image';
+import AuthForm from './components/AuthForm';
 
 export default function Home() {
   return (
-    <div
-      className="
-        flex
-        min-h-full
-        flex-col
-        justify-center
-        py-12
-        sm:px-6
-        lg:px-8
-        bg-gray-100
-      "
-    >
+    <div className="flex flex-col justify-center min-h-full py-12 bg-gray-100 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <Image
           alt="Logo"
           height={48}
           width={48}
-          className="mx-auto w-auto"
+          className="w-auto mx-auto"
           src="/images/logo.png"
         />
-        <h2
-          className="
-            mt-6
-            text-center
-            text-3xl
-            font-bold
-            trancking-tight
-            text-gray-900
-          "
-        >
+        <h2 className="mt-6 text-3xl font-bold text-center text-gray-900 trancking-tight">
           Sign in to your account
         </h2>
       </div>
-      {/* AuthForm */}
+      <AuthForm />
     </div>
   );
 }
